41f002de6d0d5d117d50e1dee181d14acbc23262
jjangdol06 <43868369+jjangdol06@users.noreply.github.com>
2019-06-17 16:54:15 +0900

    19.06.17 commit

--- app/src/main/java/com/cookandroid/myexchange/MainActivity.java
+++ app/src/main/java/com/cookandroid/myexchange/MainActivity.java
@@ -1,46 +1,113 @@
 package com.cookandroid.myexchange;
 
-import android.app.TabActivity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TabHost;
 
-public class MainActivity extends TabActivity { //TabActivity를 상속받는다.
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.KakaoSDK;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
 
-    //Button btn1, btnGo, btnP;
-    //EditText edtUrl;
-    //WebView web;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "";
+    SessionCallback callback;
+    Button btLogOut;
+    Button login;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("환전+여행정보");
 
-        TabHost tabHost = getTabHost();
+        /**카카오톡 로그아웃 요청**/
+        //한번 로그인이 성공하면 세션 정보가 남아있어서 로그인창이 뜨지 않고 바로 onSuccess()메서드를 호출하므로
+        // 매번 로그아웃을 요청함
+        /*
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                //로그아웃 성공 후 하고싶은 내용 코딩 ~
+            }
+        });*/
 
-        //TabSpec은 LinearLayout의 갯수만큼 필요하다.
-        TabHost.TabSpec tabSsong = tabHost.newTabSpec("지도").setIndicator("지도");
-        tabSsong.setContent(R.id.map);
-        tabHost.addTab(tabSsong);
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+    }
 
-        TabHost.TabSpec tabSartist = tabHost.newTabSpec("환율").setIndicator("환율");
-        tabSartist.setContent(R.id.exchange);
-        tabHost.addTab(tabSartist);
+    class SessionCallback implements ISessionCallback {
 
-        TabHost.TabSpec tabSalbum = tabHost.newTabSpec("커뮤니티").setIndicator("커뮤니티");
-        tabSalbum.setContent(R.id.webcafe);
-        tabHost.addTab(tabSalbum);
+        @Override
+        public void onSessionOpened() {
 
-        TabHost.TabSpec tabSweb = tabHost.newTabSpec("마이페이지").setIndicator("마이페이지");
-        tabSweb.setContent(R.id.myPage);
-        tabHost.addTab(tabSweb);
+            UserManagement.requestMe(new MeResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
 
-        tabHost.setCurrentTab(1); //첫화면을 결정해준다.
-    } //onCreate
+                    ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                    if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                        //에러로 인한 로그인 실패
+                        // finish();
+                    } else {
+                        //redirectMainActivity();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                }
+
+                @Override
+                public void onNotSignedUp() {
+                }
+
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    //로그인에 성공하면 로그인한 사용자의 일련번호, 닉네임, 이미지url등을 리턴합니다.
+                    //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
+
+                    Log.e("UserProfile", userProfile.toString());
+                    Log.e("UserProfile", userProfile.getId() + "");
+                    Log.e("UserProfile", userProfile.getProfileImagePath() + "");
+                    Log.e("UserProfile", String.valueOf(userProfile.getId()) + "");
+                    Log.e("UserProfile", userProfile.getNickname() + "");
+
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+        }
+        // 세션 실패시
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+        }
+    }
 }

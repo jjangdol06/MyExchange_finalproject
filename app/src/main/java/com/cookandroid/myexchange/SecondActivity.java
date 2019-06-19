ed32e7e471bc0b340b18ad2140bbbf8baa3e30c1
jjangdol06 <43868369+jjangdol06@users.noreply.github.com>
2019-06-19 12:44:52 +0900

    19.06.19

--- app/src/main/java/com/cookandroid/myexchange/SecondActivity.java
+++ app/src/main/java/com/cookandroid/myexchange/SecondActivity.java
@@ -16,9 +16,12 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,11 +50,15 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
     GoogleMap gMap;
     MapFragment mapFrag;
     GroundOverlayOptions videoMark;
-    //ListView listview;
     String tag = "country_alert";
     EditText search_country;
     Button search_country_button;
     TextView single_result;
+
+    ListView listview;
+    ListViewAdapter adapter;
+
+    Spinner countrylist1, countrylist2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +71,28 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
         TabHost.TabSpec tabSmap = tabHost.newTabSpec("지도").setIndicator("지도");
         tabSmap.setContent(R.id.map);
         tabHost.addTab(tabSmap);
+        //구글맵
+        mapFrag=(MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this); //mapAsync: 서버를 가져올 때 사용 (UI 가져오면서 뒤에서 MAP을 준비함)
 
         TabHost.TabSpec tabSexchangerate = tabHost.newTabSpec("환율").setIndicator("환율");
         tabSexchangerate.setContent(R.id.exchange);
         tabHost.addTab(tabSexchangerate);
 
+        //리스트뷰(커뮤니티)
         TabHost.TabSpec tabScommunity = tabHost.newTabSpec("커뮤니티").setIndicator("커뮤니티");
         tabScommunity.setContent(R.id.webcafe);
         tabHost.addTab(tabScommunity);
+        listview = (ListView)findViewById(R.id.listview);
+        String names[]={"미국||USA","유럽||EUROP","베트남||VIETNAM","아시아||ASIA" };
+        int icons[]={R.drawable.usa, R.drawable.europe, R.drawable.vietnam, R.drawable.asia};
+        adapter = new ListViewAdapter();
+
+        for(int i =0;i<4;i++){
+            adapter.addItem(this.getResources().getDrawable(icons[i]),names[i]);
+        }
+        listview.setAdapter(adapter);
+
 
         TabHost.TabSpec tabSalert = tabHost.newTabSpec("여행 경보").setIndicator("여행 경보");
         tabSalert.setContent(R.id.Alert);
@@ -79,15 +100,12 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
 
         tabHost.setCurrentTab(1); //첫화면을 결정해준다.
 
-        //구글맵
-        mapFrag=(MapFragment)getFragmentManager().findFragmentById(R.id.map);
-        mapFrag.getMapAsync(this); //mapAsync: 서버를 가져올 때 사용 (UI 가져오면서 뒤에서 MAP을 준비함)
+       //여행경보_tabSalert
+        search_country=(EditText)findViewById(R.id.search_country); //치안 정보가 궁금한 나라 입력
+        search_country_button=(Button)findViewById(R.id.search_country_button); // 입력 버튼
+        single_result=(TextView)findViewById(R.id.alert_result); //결과를 나타내는 textview
 
-        //listview=(ListView)findViewById(R.id.listView);
-        search_country=(EditText)findViewById(R.id.search_country);
-        search_country_button=(Button)findViewById(R.id.search_country_button);
-        single_result=(TextView)findViewById(R.id.alert_result);
-
+        //입력버튼에 리스너를 단다.
         search_country_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,7 +117,24 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
                 new DownloadWebpageTask().execute(strUrl);
             }
         });
+
+        //국가 선택에 대한 Spinner(드롭다운):자체 xml 파일 생성해줌(country_dropdown)
+        String[] CountryList = {
+                "대한민국", "남아프리카 공화국", "네팔", "노르웨이", "뉴질랜드", "대만", "덴마크", "러시아", "마카오",
+                "말레이시아", "멕시코", "몽골", "미국", "바레인", "방글라데시", "베트남", "브라질", "브루나이", "사우디아라비아",
+                "스웨덴", "스위스", "싱가포르", "아랍에미리트", "영국", "오만", "요르단", "유럽연합", "이스라엘","이집트", "인도",
+                "인도네시아", "일본", "중국", "체코", "칠레", "카자흐스탄", "카타르", "캐나다", "쿠웨이트", "태국", "터키", "파키스탄",
+                "폴란드", "필리핀", "헝가리", "호주", "홍콩"};
+        countrylist1 = (Spinner)findViewById(R.id.country1);
+        countrylist2 = (Spinner)findViewById(R.id.country2);
+        ArrayAdapter<String> cadapter = new ArrayAdapter<String>(this, R.layout.country_dropdown, CountryList);
+        countrylist1.setAdapter(cadapter);
+        countrylist1.setSelection(0);
+        countrylist2.setAdapter(cadapter);
+
     } //onCreate
+
+    //118~238 tabSalert에 관련된 api 호출
     private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -124,6 +159,9 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
                 xpp.setInput(new StringReader(result));
                 int eventType = xpp.getEventType();
                 boolean a=false, aP=false, aN=false, c=false, cN=false, l=false, lP=false, lN=false;
+                boolean num=false, deal=false;
+                Double mid,mid2,fin;
+                String m2;
                 single_result.setText("");
                 while (eventType != XmlPullParser.END_DOCUMENT) {
                     if(eventType == XmlPullParser.START_DOCUMENT) {
@@ -146,6 +184,10 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
                             lP= true;
                         if (tag_name.equals("limitaNote"))
                             lN = true;
+                        if (tag_name.equals("CUR_NM"))
+                            num = true;
+                        if (tag_name.equals("DEAL_BAS_R"))
+                            deal = true;
                     } else if(eventType == XmlPullParser.TEXT) {
                         if (a) {
                             //String content = xpp.getText();
@@ -223,7 +265,8 @@ public class SecondActivity extends TabActivity implements OnMapReadyCallback { 
     }
 
 
-        @Override
+    //구글맵
+    @Override
     public void onMapReady(GoogleMap googleMap) { //MAP이 준비되면 Async가 자동으로 부름
         gMap = googleMap;
         gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
